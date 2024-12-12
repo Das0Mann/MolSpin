b3bbdbb27a55a63ece77edd3b952cc21dbb78460
Das0Mann <luca2193@yahoo.de>
2024-12-12 14:06:10 +0100

    Fixed some bugs in NZ and HSDIRECTYIELD

--- RunSection/Tasks/TaskStaticSSNakajimaZwanzig.cpp
+++ RunSection/Tasks/TaskStaticSSNakajimaZwanzig.cpp
@@ -465,7 +465,7 @@ namespace RunSection
 
 									for (int l = 0; l < num_element; l++)
 									{
-										ptr_SpecDens[l] = new arma::cx_mat(H);
+										ptr_SpecDens[l] = new arma::cx_mat(lambda);
 										*ptr_SpecDens[l] *= 0.0;
 									}
 
@@ -795,7 +795,7 @@ namespace RunSection
 
 										for (int l = 0; l < num_element; l++)
 										{
-											ptr_SpecDens[l] = new arma::cx_mat(H);
+											ptr_SpecDens[l] = new arma::cx_mat(lambda);
 											*ptr_SpecDens[l] *= 0.0;
 										}
 
