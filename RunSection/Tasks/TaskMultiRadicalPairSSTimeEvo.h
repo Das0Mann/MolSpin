612f4ca465db4d51dcfa5205d891dcabfd98d050
bptigg <bptigg@gmail.com>
2025-01-15 18:40:39 +0000

    added the direct solver to the task

--- RunSection/Tasks/TaskMultiRadicalPairSSTimeEvo.h
+++ RunSection/Tasks/TaskMultiRadicalPairSSTimeEvo.h
@@ -40,12 +40,13 @@ namespace RunSection
 		double totaltime;
 		SpinAPI::ReactionOperatorType reactionOperators;
 
-		void WriteHeader(std::ostream &); // Write header for the output file
+		void WriteHeader(std::ostream &, bool yield = true); // Write header for the output file
 
 		// Private method that gathers and outputs the results from a given time-integrated density operator
 		void GatherResults(const arma::cx_mat &, const SpinAPI::SpinSystem &, const SpinAPI::SpinSpace &, std::vector<std::complex<double>>& traj);
 		
 		void StateYield(double, double&, const std::vector<std::complex<double>>&, std::vector<double>& ); //Method that calculates the yeild for a given state
+		void StateYield(double, double&, int, int, arma::cx_mat&, arma::cx_vec&); //rate, yield (to be retunred), spinsystem, projection operator, state density vec
 		double simpson_integration(std::vector<double> x_list, std::vector<double> y_list);
 		
 		bool GenerateHamiltonian(const std::vector<SpinAPI::interaction_ptr> interactions, arma::sp_cx_mat& H, int dimension, std::shared_ptr<SpinAPI::SpinSpace> SpinSystem);
@@ -54,9 +55,11 @@ namespace RunSection
 		bool ValidateSubSystems();
 
 		//RungeKutta4 method 
-		bool RungeKutta4(arma::sp_cx_mat& L, arma::cx_vec& RhoNaught, arma::cx_vec& drhodt, double timestep);
+		//bool RungeKutta4(arma::sp_cx_mat& L, arma::cx_vec& RhoNaught, arma::cx_vec& drhodt, double timestep);
 		//static Matrix ComputeRhoDot(Matrix& L, Matrix& K, Matrix RhoNaugt);  //k in this case isn't refering to the reaction operator K but k_i, i E {1,2,3,4} for the RK4 method
 		static arma::cx_vec ComputeRhoDot(double t, arma::sp_cx_mat& L, arma::cx_vec& K, arma::cx_vec RhoNaught);
+		
+		void CalcYieldOnly(arma::sp_cx_mat& L, arma::cx_vec& RhoNaught, arma::cx_vec& ReturnVec); //L matrix, RhoNaught and a vector to return the data
 
 	protected:
 		bool RunLocal() override;
